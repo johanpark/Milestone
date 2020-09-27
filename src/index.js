a1e84dd7c17c36460d28a5b675b82dadf60ee51f
pyhan23@gmail.com <pyhan23@gmail.com>
2020-09-27 22:46:44 +0900

    9/27 Auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'component/App';
 
 
 ReactDOM.render(
@@ -10,7 +10,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
