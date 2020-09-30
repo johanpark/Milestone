f529ef1500ddd5ea0bffd33c2625623acfc01993
pyhan23@gmail.com <pyhan23@gmail.com>
2020-09-30 13:51:08 +0900

    9/30 maps

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
- 
-class Maps extends Component{
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+        
+class Maps extends React.Component{
     render(){
         const mapStyles = {
             width: '100%',
             height: '100%',
           };
+        
+        let positionstate ={
+          lat : 0,
+          lng : 0,
+         
+        };
+
+       componentDidMount = () =>{
+          if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(successGetCurrentPosition);
+          }else{
+            console.log("Fail");
+          }
+        };
+      const successGetCurrentPosition = (position) => {
+        let setpositionstate =() =>{
+          ...positionstate,
+          lat : position.coords.latitude,
+          lng : position.coords.longitude,
+          
+        };
+      };
         return(
-            <div>
+            <div className={"FindAddressPresenter"}>
                 <Map
                 google={this.props.google}
                 zoom={15}
-                initialCenter={{lat: 37.5, lng : 127}}
+                center={{positionstate}}
+                initialCenter={{positionstate}}
                 style={mapStyles}
                 >
+                  <Marker name={"Current location"} />
+                  <InfoWindow>
+                    <div>
+                      <h1>test</h1>
+                    </div>
+                  </InfoWindow>
                 </Map>
             </div>
         );
     }
+    
 }
   export default GoogleApiWrapper({
-    apiKey: '..'
+    apiKey: 'AIzaSyA07KpTijPFrEBp3vMgSNQjtZrqNYeSgXk'
   })(Maps)
