18e5a122cf47acd75e78aabcb838bee756b2d6f2
johanpark <pyhan23@gmail.com>
2020-10-05 15:56:19 +0900

    10/5 now map current location

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,55 +1,62 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
-        
-class Maps extends React.Component{
-    render(){
-        const mapStyles = {
-            width: '100%',
-            height: '100%',
-          };
-        
-        let positionstate ={
-          lat : 0,
-          lng : 0,
-         
-        };
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
+import React from "react";
 
-       componentDidMount = () =>{
-          if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(successGetCurrentPosition);
-          }else{
-            console.log("Fail");
-          }
-        };
-      const successGetCurrentPosition = (position) => {
-        let setpositionstate =() =>{
-          ...positionstate,
-          lat : position.coords.latitude,
-          lng : position.coords.longitude,
-          
-        };
-      };
-        return(
-            <div className={"FindAddressPresenter"}>
-                <Map
-                google={this.props.google}
-                zoom={15}
-                center={{positionstate}}
-                initialCenter={{positionstate}}
-                style={mapStyles}
-                >
-                  <Marker name={"Current location"} />
-                  <InfoWindow>
-                    <div>
-                      <h1>test</h1>
-                    </div>
-                  </InfoWindow>
-                </Map>
-            </div>
-        );
+class FindAddressPresenter extends React.Component {
+   state = {
+    lat: 0,
+    lng: 0,
+    loading: true
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.successGetCurrentPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser");
     }
-    
+  }
+
+   render() {
+    const { lat, lng, loading } = this.state;
+
+    if (loading) {
+      return "loading...";
+    }
+    return (
+      <div className={"FindAddressPresenter"}>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          center={{
+            lat,
+            lng
+          }}
+          initialCenter={{
+            lat,
+            lng
+          }}
+        >
+          <Marker name={"Current location"} />
+          <InfoWindow>
+            <div>
+              <h1>test</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
+
+   successGetCurrentPosition = position => {
+    this.setState({
+      ...this.state,
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+      loading: false
+    });
+  };
 }
-  export default GoogleApiWrapper({
-    apiKey: '..'
-  })(Maps)
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA07KpTijPFrEBp3vMgSNQjtZrqNYeSgXk'
+})(FindAddressPresenter);
--- src/fbase.js
+++ src/fbase.js
@@ -1,13 +1,21 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-    appId: process.env.REACT_APP_APP_ID
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_DATABASE_URL,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    // appId: process.env.REACT_APP_APP_ID
+
+    apiKey: "AIzaSyAEfvPoozH1Dr_KhFGyx22ZbUCGQgudD_o",
+    authDomain: "milestone-f40ce.firebaseapp.com",
+    databaseURL: "https://milestone-f40ce.firebaseio.com",
+    projectId: "milestone-f40ce",
+    storageBucket: "milestone-f40ce.appspot.com",
+    messagingSenderId: "923884553918",
+    appId: "1:923884553918:web:22c13b3070b7f67c3480ef"
   };
   firebase.initializeApp(firebaseConfig);
 
